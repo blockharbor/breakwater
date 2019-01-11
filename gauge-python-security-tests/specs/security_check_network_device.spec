f66490fbf2b87630c4e077ab402e709eadefe226
Brandon Barry <brandonbarry@Brandons-MacBook-Pro-2.local>
2019-01-11 15:27:14 -0500

    Begin adding support for Nessus via Tenable.io API.

---
+++ gauge-python-security-tests/specs/security_check_network_device.spec
@@ -0,0 +1,18 @@
+# Specification Heading
+
+This is an executable specification file. This file follows markdown syntax.
+Every heading in this file denotes a scenario. Every bulleted point denotes a step.
+
+To execute this specification, run
+
+    gauge run specs
+
+
+Verify the IP address is on the network, and port scan it. 
+
+## NMAP Scan
+
+* Scan network host: "172.22.22.22" on ports: "2-1000".
+
+
+
--- gauge-python-security-tests/step_impl/nmap_scan_host.py
+++ gauge-python-security-tests/step_impl/nmap_scan_host.py
@@ -9,10 +9,12 @@ from getgauge.python import step, before_scenario, Messages
 # --------------------------
 
 @step("Scan network host: <ipAddr> on ports: <ports>.")
-def assert_nmap(url):
+def assert_nmap(ipAddr, ports):
+    print(ipAddr, ports)
     nm = nmap.PortScanner()
-    nm.scan(ipAddr, ports)
-    assert resp.headers['X-CDN'] == "Incapsula"
+    nm.scan(str(ipAddr), str(ports))
+    print(nm.csv())
+    assert nm[str(ipAddr)].state() == "up"
 
 
 # ---------------
